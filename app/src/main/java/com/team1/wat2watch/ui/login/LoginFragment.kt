83b6ea3ec55dd7a790737052083f786b69450101
Hassan <hashmihassan47@gmail.com>
2025-02-09 12:24:29 -0500

    chore(LoginFragment): test

--- app/src/main/java/com/team1/wat2watch/ui/login/LoginFragment.kt
+++ app/src/main/java/com/team1/wat2watch/ui/login/LoginFragment.kt
@@ -1,8 +1,14 @@
 package com.team1.wat2watch.ui.login
 
 import androidx.fragment.app.Fragment
+import com.team1.wat2watch.databinding.FragmentHomeBinding
 
 
 class LoginFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+
+    private val binding get() = _binding!!
+
+
 
 }
