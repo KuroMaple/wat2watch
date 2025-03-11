3e0528f362450d705a4147a1e1c808c806765537
Hassan <hashmihassan47@gmail.com>
2025-03-10 21:01:19 -0400

    feat(profile): Refactor ProfileScreen and update build configurations

--- app/src/main/java/com/team1/wat2watch/ui/navbar/NavBar.kt
+++ app/src/main/java/com/team1/wat2watch/ui/navbar/NavBar.kt
@@ -13,7 +13,6 @@ import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -24,7 +23,6 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import androidx.navigation.compose.rememberNavController
 import com.team1.wat2watch.R
 
 @Composable
